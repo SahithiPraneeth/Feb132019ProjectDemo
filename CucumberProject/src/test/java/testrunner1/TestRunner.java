2e7c1b088d0de1e43f9cc6700a7782418c2de181
SahithiPraneeth <47525589+SahithiPraneeth@users.noreply.github.com>
2019-02-13 15:29:11 +0530

    Update and rename DemoLoginTestRunner.java to TestRunner.java

--- CucumberProject/src/test/java/testrunner1/TestRunner.java
+++ CucumberProject/src/test/java/testrunner1/TestRunner.java
@@ -10,6 +10,6 @@ import cucumber.api.junit.Cucumber;
 	glue= {"stepdefs1"}, plugin= {"pretty", "json:target/cucumber-reports/Cucumber.json"}, 
 	monochrome= true)
 
-public class DemoLoginTestRunner {
+public class TestRunner {
 
 }
